e2e8889a288d232b8a956bff15c1c54f17020459
AnkitShrivastava27 <ankitshrivastavaks@gmail.com>
2026-07-20 11:22:28 +0530

    allow

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -23,6 +23,7 @@ class Settings(BaseSettings):
     ALLOWED_ORIGINS: List[str] = [
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://ancentrixvoice-five.vercel.app"
     ]
 
     # License
